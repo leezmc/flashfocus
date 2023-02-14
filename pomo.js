fee8e2deb04e31aa8329e29484c3eb939d595e3c
Michael Lee <mzl28@drexel.edu>
2023-02-14 16:03:30 -0500

    Doc title and counting sessions

---
+++ pomo.js
@@ -0,0 +1,71 @@
+const startButton = document.getElementById("start");
+          const stopButton = document.getElementById("stop");
+          const resetButton = document.getElementById("reset");
+          const timerDisplay = document.getElementById("timer");
+          const shortBreakBtn = document.getElementById("short-break-btn");
+          const longBreakBtn = document.getElementById("long-break-btn");
+          const pomodoroBtn = document.getElementById("pomodoro-btn");
+          
+          let timer;
+          let seconds = 1500; // 25 minutes 
+          let sessions = 0 
+      
+          startButton.addEventListener("click", startTimer);
+          stopButton.addEventListener("click", stopTimer);
+          resetButton.addEventListener("click", resetTimer);
+          shortBreakBtn.addEventListener("click", startShortBreak);
+          longBreakBtn.addEventListener("click", startLongBreak);
+          pomodoroBtn.addEventListener("click", startPomodoro);
+          
+          function startTimer() {
+            timer = setInterval(function() {
+              seconds--;
+              displayTime();
+              if (seconds === 0) {
+                stopTimer();
+                alert("Time's up! Take a break!");
+                sessions++;
+                if (sessions 0 <= sesions <= 4) {
+                    startShortBreak()
+                }
+              }
+            }, 1000);
+            startButton.disabled = true;
+            stopButton.disabled = false;
+          }
+      
+          function stopTimer() {
+            clearInterval(timer);
+            startButton.disabled = false;
+            stopButton.disabled = true;
+          }
+      
+          function resetTimer() {
+            stopTimer();
+            seconds = 1500;
+            displayTime();
+          }
+
+          function startShortBreak() {
+            stopTimer();
+            seconds = 300;
+            displayTime();
+  }
+
+  function startLongBreak() {
+            stopTimer();
+            seconds = 600;
+            displayTime();
+  }
+
+  function startPomodoro() {
+            stopTimer();
+            seconds = 1500;
+            displayTime();
+  }
+      
+          function displayTime() {
+            let minutes = Math.floor(seconds / 60);
+            let remainingSeconds = seconds % 60;
+            timerDisplay.innerHTML = `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+          }
