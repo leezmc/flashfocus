48b4ac498c182793918d83a33c72b5ad2dc1318d
Michael Lee <mzl28@drexel.edu>
2023-02-23 14:11:08 -0500

    clean css

--- mainpagetotal.js
+++ mainpagetotal.js
@@ -5,15 +5,11 @@ $(window).scroll(function() {
         $body = $('body'),
         $panel = $('.panel');
     
-    // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
+// fade time
+    var scroll = $window.scrollTop() + ($window.height() / 2);
    
     $panel.each(function () {
       var $this = $(this);
-      
-      // if position is within range of this panel.
-      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-      // Remember we set the scroll to 33% earlier in scroll var.
       if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
             
         // Remove all classes on body with color-
