8d367ef554dc20af68e95bbc15c6208c957a0913
Michael Lee <mzl28@drexel.edu>
2023-02-23 13:31:59 -0500

    revamped entire page, added all pages into one singular

---
+++ pomotimer.js
@@ -0,0 +1,72 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+const timerDisplay = document.getElementById("timer");
+const shortBreakBtn = document.getElementById("short-break-btn");
+const longBreakBtn = document.getElementById("long-break-btn");
+const pomodoroBtn = document.getElementById("pomodoro-btn");
+
+const audio = new Audio();
+  audio.src = "timer-button.mp3"
+
+let timer;
+let seconds = 1500; // 25 minutes 
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
+shortBreakBtn.addEventListener("click", startShortBreak);
+longBreakBtn.addEventListener("click", startLongBreak);
+pomodoroBtn.addEventListener("click", startPomodoro);
+
+function startTimer() {
+  timer = setInterval(function() {
+    seconds--;
+    displayTime();
+
+    if (seconds === 0) {
+      stopTimer();
+      audio.play();
+      startShortBreak();
+      startTimer();
+    }
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopTimer() {
+  clearInterval(timer);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function resetTimer() {
+  stopTimer();
+  seconds = 1500;
+  displayTime();
+}
+
+function startShortBreak() {
+  stopTimer();
+  seconds = 300;
+  displayTime();
+}
+
+function startLongBreak() {
+  stopTimer();
+  seconds = 600;
+  displayTime();
+}
+
+function startPomodoro() {
+  stopTimer();
+  seconds = 1500;
+  displayTime();
+}
+
+function displayTime() {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  timerDisplay.innerHTML = `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+}
